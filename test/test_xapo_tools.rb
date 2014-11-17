528da63af79815bdcac6e46d0373bc4948d6dbe4
Federico Repond <frepond@gmail.com>
2014-11-17 08:15:38 -0300

    Fixed tools test.

--- test/test_xapo_tools.rb
+++ test/test_xapo_tools.rb
@@ -4,9 +4,9 @@ require "xapo_tools"
 class TestXapoTools < Minitest::Test
   def setup     
     @xapo_tools = XapoTools::MicroPayment.new(
-      "https://mpayment.xapo.com/pay_button/show", 
-      "b91014cc28c94841", 
-      "c533a6e606fb62ccb13e8baf8a95cbdc"
+      "https://mpayment.xapo.com/pay_button/show",  
+      "your app id", 
+      "your app secret"
     )
     @xapo_tools_notpa = XapoTools::MicroPayment.new(
       "https://mpayment.xapo.com/pay_button/show"
@@ -23,11 +23,12 @@ class TestXapoTools < Minitest::Test
     config[:amount_BIT] = 0.01
     config[:pay_type] = PayType::DONATE
     config[:reference_code] = "test"
+    config[:end_mpayment_uri] = "http://localhost:9000"
+    config[:redirect_uri] = "http://localhost:9000"
 
     customization = XapoTools.micro_payment_customization
     customization[:predefined_pay_values] = "1,5,10"
-    customization[:end_mpayment_uri] = "http://localhost:9000"
-    customization[:redirect_uri] = "http://localhost:9000"
+    customization[:login_cellphone_header_title] = "Test MicroPayment"
     customization[:button_css] = "grey"
 
     actual = @xapo_tools.build_iframe_widget(config, customization)
@@ -46,11 +47,12 @@ class TestXapoTools < Minitest::Test
     config[:amount_BIT] = 0.01
     config[:pay_type] = PayType::DONATE
     config[:reference_code] = "test"
+    config[:end_mpayment_uri] = "http://localhost:9000"
+    config[:redirect_uri] = "http://localhost:9000"
 
     customization = XapoTools.micro_payment_customization
     customization[:predefined_pay_values] = "1,5,10"
-    customization[:end_mpayment_uri] = "http://localhost:9000"
-    customization[:redirect_uri] = "http://localhost:9000"
+    customization[:login_cellphone_header_title] = "Test MicroPayment"
     customization[:button_css] = "red"
 
     actual = @xapo_tools_notpa.build_iframe_widget(config, customization)
@@ -69,6 +71,13 @@ class TestXapoTools < Minitest::Test
     config[:amount_BIT] = 0.01
     config[:pay_type] = PayType::DONATE
     config[:reference_code] = "test"
+    config[:end_mpayment_uri] = "http://localhost:9000"
+    config[:redirect_uri] = "http://localhost:9000"
+
+    customization = XapoTools.micro_payment_customization
+    customization[:predefined_pay_values] = "1,5,10"
+    customization[:login_cellphone_header_title] = "Test MicroPayment"
+    customization[:button_css] = "grey"
 
     actual = @xapo_tools.build_iframe_widget(config)
     puts("test_build_iframe_widget -> ", actual)
@@ -86,12 +95,13 @@ class TestXapoTools < Minitest::Test
     config[:amount_BIT] = 0.01
     config[:pay_type] = PayType::TIP
     config[:reference_code] = "test"
+    config[:end_mpayment_uri] = "http://localhost:9000"
+    config[:redirect_uri] = "http://localhost:9000"
 
     customization = XapoTools.micro_payment_customization
     customization[:predefined_pay_values] = "1,5,10"
-    customization[:end_mpayment_uri] = "http://localhost:9000"
-    customization[:redirect_uri] = "http://localhost:9000"
-    customization[:button_css] = "grey"
+    customization[:login_cellphone_header_title] = "Test MicroPayment"
+    customization[:button_css] = "red"
 
     regex = /
                 <div\sid="tipButtonDiv"\sclass="tipButtonDiv"><\/div>\n
@@ -115,12 +125,13 @@ class TestXapoTools < Minitest::Test
     config[:amount_BIT] = 0.01
     config[:pay_type] = PayType::TIP
     config[:reference_code] = "test"
+    config[:end_mpayment_uri] = "http://localhost:9000"
+    config[:redirect_uri] = "http://localhost:9000"
 
     customization = XapoTools.micro_payment_customization
     customization[:predefined_pay_values] = "1,5,10"
-    customization[:end_mpayment_uri] = "http://localhost:9000"
-    customization[:redirect_uri] = "http://localhost:9000"
-    customization[:button_css] = "red"
+    customization[:login_cellphone_header_title] = "Test MicroPayment"
+    customization[:button_css] = "grey"
 
     regex = /
                 <div\sid="tipButtonDiv"\sclass="tipButtonDiv"><\/div>\n
@@ -129,29 +140,6 @@ class TestXapoTools < Minitest::Test
             /mx
 
     actual = @xapo_tools_notpa.build_div_widget(config, customization)
-    puts("test_build_div_widget_notpa -> ", actual)
-
-    assert_match(regex, actual)
-  end
-
-  def test_build_div_widget_cust()
-    config = XapoTools.micro_payment_config
-    config[:sender_user_email] = "sender@xapo.com"
-    config[:sender_user_cellphone] = "+5491112341234"
-    config[:receiver_user_id] = "r0210"
-    config[:receiver_user_email] = "fernando.taboada@xapo.com"
-    config[:pay_object_id] = "to0210"
-    config[:amount_BIT] = 0.01
-    config[:pay_type] = PayType::TIP
-    config[:reference_code] = "test"
-
-    regex = /
-                <div\sid="tipButtonDiv"\sclass="tipButtonDiv"><\/div>\n
-                <div\sid="tipButtonPopup"\sclass="tipButtonPopup"><\/div>\n
-                <script>(.*)payload(.*)<\/script>\n
-            /mx
-
-    actual = @xapo_tools_notpa.build_div_widget(config)
     puts("test_build_div_widget_notpa -> ", actual)
 
     assert_match(regex, actual)
